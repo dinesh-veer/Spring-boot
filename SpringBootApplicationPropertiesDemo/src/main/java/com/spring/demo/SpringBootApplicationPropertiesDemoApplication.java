6d169f59a9fe0ae2f9febdb309747c82a6d21b9c
dineshveer <dveer123@hotmail.com>
2020-05-31 12:57:35 +0530

    "Added PROD config file and updated command in main file"

--- SpringBootApplicationPropertiesDemo/src/main/java/com/spring/demo/SpringBootApplicationPropertiesDemoApplication.java
+++ SpringBootApplicationPropertiesDemo/src/main/java/com/spring/demo/SpringBootApplicationPropertiesDemoApplication.java
@@ -34,3 +34,5 @@ public class SpringBootApplicationPropertiesDemoApplication {
 //TO activate differernt profile sample Dev properties file
 //use below command on generated jar
 // java -jar JARNAME --spring.profiles.active=dev
+//To activate PROD profile use below command
+//java -jar JARNAME --spring.profiles.active=prod
