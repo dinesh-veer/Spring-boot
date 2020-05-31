4485c924767366457cf9073dae6d552cfb18b88a
dineshveer <dveer123@hotmail.com>
2020-05-31 16:13:30 +0530

    Spring boot Logback logging application example

---
+++ SpringBootLoggingLogBackDemo/src/main/java/com/example/controller/LoggingController.java
@@ -0,0 +1,23 @@
+package com.example.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class LoggingController {
+
+	Logger logger = LoggerFactory.getLogger(LoggingController.class);
+	
+	@RequestMapping("/")
+	public String getLoggingController() {
+		logger.trace("Trace Message");
+		logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+		return "In Logging controller";
+	}
+}
