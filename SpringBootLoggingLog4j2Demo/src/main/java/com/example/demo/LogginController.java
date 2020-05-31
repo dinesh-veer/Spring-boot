aa1da04e85c60939112bda695ae78a3d83291c8e
dineshveer <dveer123@hotmail.com>
2020-05-31 16:34:50 +0530

    Added spring boot logging using Log4j2

---
+++ SpringBootLoggingLog4j2Demo/src/main/java/com/example/demo/LogginController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class LogginController {
+
+	Logger logger = LoggerFactory.getLogger(LogginController.class);
+	
+	@RequestMapping
+	public String getMessage() {
+		logger.trace("Trace Message");
+		logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+		return "Test";
+	}
+}
---
+++ SpringBootLoggingLog4j2Demo/src/main/java/com/example/demo/SpringBootLoggingLog4j2DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoggingLog4j2DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLoggingLog4j2DemoApplication.class, args);
+	}
+
+}
