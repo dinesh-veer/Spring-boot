b7868957a3adcd84fe2bd9ef80df194249e82c1a
dineshveer <dveer123@hotmail.com>
2020-06-03 10:08:31 +0530

    Added spring boot thymeleaf implementation for html pages

---
+++ SpringBootThymeleafDemo/src/main/java/com/springBoot/demo/SpringBootThymeleafDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThymeleafDemoApplication.class, args);
+	}
+
+}
---
+++ SpringBootThymeleafDemo/src/main/java/com/springBoot/demo/controller/WelcomeController.java
@@ -0,0 +1,36 @@
+package com.springBoot.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping("/index")
+	public String welcomePage() {
+		return "index";
+	}
+}
+/**
+ * 
+To run the JSP page:
+
+To have this in pom.xml
+
+<!-- JSP -->
+<dependency>
+    <groupId>org.apache.tomcat.embed</groupId>
+    <artifactId>tomcat-embed-jasper</artifactId>
+    <scope>provided</scope>
+</dependency>
+<!-- jstl for jsp -->
+<dependency>
+    <groupId>javax.servlet</groupId>
+    <artifactId>jstl</artifactId>
+</dependency>
+It may be not enough.
+
+You must not miss this.
+
+<packaging>war</packaging>
+**/
