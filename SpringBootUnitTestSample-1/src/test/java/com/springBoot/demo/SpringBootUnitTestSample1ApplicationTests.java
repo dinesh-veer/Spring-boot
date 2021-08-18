e2ad70555b64f76ff53310d526b02890e9bf16e9
dineshveer <dveer123@hotmail.com>
2021-08-18 12:10:19 +0530

    Updated code

---
+++ SpringBootUnitTestSample-1/src/test/java/com/springBoot/demo/SpringBootUnitTestSample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootUnitTestSample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
