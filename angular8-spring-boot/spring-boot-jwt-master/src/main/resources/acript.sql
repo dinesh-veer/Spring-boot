e098e9fd0e6afeaf05215d8854d9f8f51bb99077
dineshveer <dveer123@hotmail.com>
2021-08-18 11:51:48 +0530

    updated code

---
+++ angular8-spring-boot/spring-boot-jwt-master/src/main/resources/acript.sql
@@ -0,0 +1,3 @@
+INSERT INTO User (id, firstname, lastname, username, password, salary, age) VALUES (1, 'Devglan','Devglan', 'devglan','$2y$04$wuElMwi4H/Cct6P4mRYuROMBPqjTiV6Lk9vh7xgwDzd2mRO98wpH.', 3456, 33);
+INSERT INTO User (id, firstname, lastname, username, password, salary, age)  VALUES (2, 'Tom', 'Asr', 'tom234', '$2a$04$QED4arFwM1AtQWkR3JkQx.hXxeAk/G45NiRd3Q4ElgZdzGYCYKZOW', 7823, 23);
+INSERT INTO User (id, firstname, lastname, username, password, salary, age)  VALUES (3, 'Adam', 'Psr', 'adam', '$2a$04$WeT1SvJaGjmvQj34QG8VgO9qdXecKOYKEDZtCPeeIBSTxxEhazNla', 4234, 45);
