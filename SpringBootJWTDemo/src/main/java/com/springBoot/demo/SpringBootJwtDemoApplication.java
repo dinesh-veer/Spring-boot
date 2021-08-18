0057d037eec3a96b3d4e0ac1807f9ff44197323f
dineshveer <dveer123@hotmail.com>
2021-08-18 11:55:31 +0530

    "added new code"

---
+++ SpringBootJWTDemo/src/main/java/com/springBoot/demo/SpringBootJwtDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springBoot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJwtDemoApplication.class, args);
+	}
+
+}
